8a610dcb03c53a828b7b817b540697b9dc6d6b9d
jtlemkin <jtlemkin@gmail.com>
2019-07-25 15:34:42 -0700

    fixes #1

--- class.java
+++ class.java
@@ -8,7 +8,7 @@ public class Class {
 
         System.out.println("The result of the next computation should be 5");
 
-        int d = (a + b);
+        int d = (c + b);
 
         System.out.println(d);
     }

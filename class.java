f30efb54fb424508855072daf69030c856add29d
jtlemkin <jtlemkin@gmail.com>
2019-07-25 15:27:47 -0700

    initial commit

---
+++ class.java
@@ -0,0 +1,16 @@
+public class Class {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        System.out.println("The result of the next computation should be 5");
+
+        int d = a + b;
+
+        System.out.println(d);
+    }
+
+}

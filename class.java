3c40c68bcff1b1ac001fc7f3bc8ca0c040adc3d7
jtlemkin <jtlemkin@gmail.com>
2019-07-29 12:12:41 -0700

    added d to line

--- class.java
+++ class.java
@@ -8,7 +8,7 @@ public class Class {
 
         System.out.println("The result of the next computation should be 5");
 
-        int d = (a + b + c);
+        int d = (a + b + c + d);
 
         System.out.println(d);
     }
